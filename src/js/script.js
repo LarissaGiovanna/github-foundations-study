afa1d551bf7e2646f3ef9b8674242a376b1e99e0
LarissaGiovanna <lgiova544@gmail.com>
2026-01-14 20:21:16 -0300

    feat: module one - add content on introduction to git and github - style - javascript integration on sections between the module

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,26 @@ function openMenu(){
         menuNav.style.display = 'block';
     }
 }
+
+//navegação entre seções
+function back(current, previous){
+    if (current && previous) {
+        let currentSection = document.getElementById(current);
+        currentSection.style.display = 'none';
+
+        let previousSection = document.getElementById(previous);
+        previousSection.style.display = 'block';
+        previousSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+function next(current, next){
+    if (current && next) {
+        let currentSection = document.getElementById(current);
+        currentSection.style.display = 'none';
+
+        let nextSection = document.getElementById(next);
+        nextSection.style.display = 'block';
+        nextSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
